bc89e679cc0a6a50f52a1972c359dddfcd6640c2
m-gebhart <michael1998gebhart@gmail.com>
2020-01-06 04:35:53 +0100

    timestamps, scrolling, chat-reply system

--- assets/scripts/chatScript.js
+++ assets/scripts/chatScript.js
@@ -1,7 +1,11 @@
 var chatBody;
 var statusLine;
-var tempMessage = '';
-var messageTime = 2;
+var inputText;
+var onlineAfter = 5;
+var tempInput = '';
+var tempBubble;
+var latestPlayerMessage;
+var messageTime = 1;
 var tempWidth = 0;
 var pxWidthPerLetter = 10;
 var pxOffsetWidth = 6;
@@ -9,16 +13,26 @@ var pxHeightPerLine = 24;
 var textPadding = "5px";
 var bubbleSideMargin = "1.5vw";
 var firstMessage = true;
+var sessionStarted = false;
+var sessionOver = false;
+var sessionProgression = 1;
 
 window.onload = function start() {
     chatBody = document.getElementById("chatBody");
     statusLine = document.getElementById("profileStatus");
+    inputText = document.getElementById("inputField");
+    sleep(onlineAfter * 1000).then(() => {
+        statusLine.innerHTML = "Online";
+    })
     load_txtHistory();
-    set_scrollBar()
+    sleep(1000).then(() => {
+        set_scrollBar()
+        sessionStarted = true;
+    })
 }
 
 function set_scrollBar() {
-    sleep(100).then(() => {
+    sleep(10).then(() => {
         chatBody.lastChild.scrollIntoView({ behavior: 'smooth', inline: 'nearest', block: 'start' });
     })
 }
@@ -26,27 +40,31 @@ function set_scrollBar() {
 function onEnter_sendMessage(event) {
     if (event.keyCode == 13)
         send_playerMessage();
-    set_scrollBar();
 }
 
 function send_playerMessage() {
-    var inputText = document.getElementById("inputField");
-    tempMessage = String(inputText.value);
-    inputText.value = '';
-    if (firstMessage)
-        create_timeStamp("today");
-    firstMessage = false;
-    create_bubble(tempMessage, true);
-    check_recentMessage();
+    if (statusLine.innerHTML != "<i>Writing...</i>" && !sessionOver && inputText.value != '') {
+        tempInput = String(inputText.value);
+        inputText.value = '';
+        if (firstMessage)
+            create_timeStamp("today");
+        firstMessage = false;
+        create_bubble(tempInput, true);
+    }
 }
 
-function send_npcMessage() {
-    var textStr = load_text();
-    create_bubble(textStr, false);
-}
-
-function load_text() {
-    return "Hey, man, long time no see! how are you?";
+function send_npcMessages(messageArray, messageInt) {
+    statusLine.innerHTML = "<i>Writing...</i>";
+    if (messageInt < messageArray.length) {
+        sleep((messageArray[messageInt].length) * 100).then(() => {
+            create_bubble(messageArray[messageInt], false);
+            send_npcMessages(messageArray, ++messageInt);
+        })
+    }
+    else {
+        statusLine.innerHTML = "Online";
+        sessionProgression++;
+    }
 }
 
 function create_timeStamp(timeStr) {
@@ -55,10 +73,15 @@ function create_timeStamp(timeStr) {
 }
 
 function create_bubble(textStr, isByPlayer) {
-    var tempBubble = create_divElement(textStr, "messageBubble");
+    tempBubble = create_divElement(textStr, "messageBubble");
     set_size(tempBubble, tempBubble.innerHTML);
     set_direction(tempBubble, isByPlayer);
     chatBody.appendChild(tempBubble);
+    if (isByPlayer && sessionStarted) {
+        latestPlayerMessage = tempBubble;
+        load_txtChatSession(sessionProgression);
+    }
+    set_scrollBar();
 }
 
 function create_divElement(textStr, cssClass) {
@@ -83,30 +106,14 @@ function calculate_height(element) {
 
 function set_direction(bubbleElement, isByPlayer) {
     if (!isByPlayer) {
-        bubbleElement.style.float = "left";
-        bubbleElement.style.marginRight = "100%";
+        bubbleElement.classList.toggle("npcBubble", true);
         bubbleElement.style.marginLeft = bubbleSideMargin;
-        bubbleElement.style.backgroundColor = "white";
-        bubbleElement.style.textAlign = "left";
         bubbleElement.style.paddingLeft = textPadding;
     }
     else if (isByPlayer) {
-        bubbleElement.style.float = "right";
-        bubbleElement.style.marginLeft = "100%";
+        bubbleElement.classList.toggle("playerBubble", true);
         bubbleElement.style.marginRight = bubbleSideMargin;
-        bubbleElement.style.backgroundColor = "rgb(140, 251, 162)";
-        bubbleElement.style.textAlign = "right";
         bubbleElement.style.paddingRight = textPadding;
-    }
-}
-
-function check_recentMessage() {
-    if (tempMessage.includes("ophie")) {
-        statusLine.innerHTML = "<i>Writing...</i>";
-        sleep(messageTime * 1000).then(() => {
-            send_npcMessage();
-            statusLine.innerHTML = "Online";
-        })
     }
 }
 
@@ -114,15 +121,31 @@ const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function load_txtFile(fileName) {
+function load_txtChatSession(sessionInt) {
     var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "assets/txt/" + String(fileName), true);
+    txtFile.open("GET", "assets/txt/session_" + String(sessionInt) + ".txt", true);
     txtFile.onreadystatechange = function () {
         if (txtFile.readyState === 4 && txtFile.status == 200) {
-            content = txtFile.responseText;
+            parse_chatSession(txtFile.responseText);
         }
     }
     txtFile.send(null);
+}
+
+function parse_chatSession(txtContent) {
+    var keywords = txtContent.split(">> KEYWORDS: [[")[1].split("]]")[0].split(", ");
+    var npcMessages = txtContent.split(">> KEYWORDS: [[")[1].split("]]")[1].split(">> ");
+    if (check_keyword(tempInput, keywords))
+        send_npcMessages(npcMessages, 1);
+    else 
+        latestPlayerMessage.style.backgroundColor = "red";
+}
+
+function check_keyword(message, keywords) {
+    for (var element = 0; element < keywords.length; element++)
+        if (message.includes(String(keywords[element])))
+            return true;
+    return false;
 }
 
 function load_txtHistory() {
