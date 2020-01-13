5f1a186ab711cbef5db8bdca974211dbba63187f
m-gebhart <45672199+m-gebhart@users.noreply.github.com>
2020-01-13 21:59:37 +0100

    finalized chat 01

--- assets/scripts/chatScript.js
+++ assets/scripts/chatScript.js
@@ -6,10 +6,8 @@ var tempInput = '';
 var latestPlayerMessage;
 var messageTime = 1.5;
 var deleteTransitionTime = 2;
-var tempWidth = 0;
 var pxWidthPerLetter = 11;
 var pxOffsetWidth = 6;
-var pxHeightPerLine = 25;
 var textPadding = "5px";
 var bubbleSideMargin = "1.5vw";
 var firstMessage = true;
@@ -20,29 +18,41 @@ var cooldown = false;
 var cooldownTime = 1;
 var inSession = false;
 
+//Start()-function, loading past messages (chatHistory.txt) and init session
 window.onload = function start() {
     chatBody = document.getElementById("chatBody");
     statusLine = document.getElementById("profileStatus");
     inputText = document.getElementById("inputField");
     sleep(onlineAfter * 1000).then(() => {
         statusLine.innerHTML = "Online";
-    })
+    });
     load_txtHistory();
     sleep(800).then(() => {
-        set_scrollBar()
+        set_scrollBar();
         inSession = true;
-    })
+    });
 }
 
+//sleep, for timed animations
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+//chatWindow always scrolls to bottom after each message
 function set_scrollBar() {
     var scrollBehavior;
     if (inSession)
         scrollBehavior = 'smooth';
     else
         scrollBehavior = 'auto';
+
     sleep(10).then(() => {
-        chatBody.lastChild.scrollIntoView({ behavior: scrollBehavior, inline: 'nearest', block: 'start' });
-    })
+        chatBody.lastChild.scrollIntoView({
+            behavior: scrollBehavior,
+            inline: 'nearest',
+            block: 'start'
+        });
+    });
 }
 
 function onEnter_sendMessage(event) {
@@ -50,9 +60,9 @@ function onEnter_sendMessage(event) {
         send_playerMessage();
 }
 
+//print message typed by player
 function send_playerMessage() {
-    if (!isWriting && progressionInt < maxSessionsInt + 1) {
-        start_cooldown();
+    if (!isWriting && progressionInt < maxSessionsInt + 1 && inputText.value != "") {
         tempInput = String(inputText.value);
         inputText.value = '';
         if (firstMessage) {
@@ -63,45 +73,19 @@ function send_playerMessage() {
     }
 }
 
-function start_cooldown() {
-    cooldown = true;
-    sleep(cooldownTime * 1000).then(() => {
-        cooldown = false;
-    })
-}
-
-function send_npcMessages(messageArray, messageInt) {
-    sleep(((messageTime - Math.random()) + (messageTime + Math.random())) * 500).then(() => {
-            statusLine.innerHTML = "<i>Writing...</i>";
-            if (messageInt < messageArray.length) {
-                sleep((messageArray[messageInt].length) * 1).then(() => {
-                    create_bubble(messageArray[messageInt], false);
-                    //recursive function
-                    send_npcMessages(messageArray, ++messageInt);
-                })
-            }
-            else {
-                isWriting = false;
-                statusLine.innerHTML = "Online";
-                sleep(onlineAfter * 1000).then(() => {
-                    statusLine.innerHTML = "";
-                })
-            }
-        })
-}
-
 function create_timeStamp(timeStr) {
     var timestampBubble = create_divElement(timeStr, "timeBubble");
     chatBody.appendChild(timestampBubble);
 }
 
+//create message element
 function create_bubble(textStr, isByPlayer) {
     var tempBubble = create_divElement(textStr, "messageBubble");
     set_size(tempBubble, tempBubble.innerHTML);
     set_direction(tempBubble, isByPlayer);
     chatBody.appendChild(tempBubble);
     if (isByPlayer && inSession) {
-        //checking message for keyword
+        //checking player's last message for keyword
         latestPlayerMessage = tempBubble;
         check_progression(progressionInt);
     }
@@ -116,18 +100,10 @@ function create_divElement(textStr, cssClass) {
 }
 
 function set_size(bubbleElement, text) {
-    bubbleElement.style.width = String(calculate_width(text) + pxOffsetWidth + "px");
-    //bubbleElement.style.height = String(calculate_height(bubbleElement) + "px");
+    bubbleElement.style.width = String(pxWidthPerLetter * text.length + pxOffsetWidth + "px");
 }
 
-function calculate_width(textStr) {
-    return pxWidthPerLetter * textStr.length;
-}
-
-/*function calculate_height(element) {
-    return Math.ceil(element.style.maxWidth / element.style.width) * pxHeightPerLine;
-}*/
-
+//determining whether lates message should set to the left (as npc's message) or to the right (as player's message)
 function set_direction(bubbleElement, isByPlayer) {
     if (!isByPlayer) {
         bubbleElement.classList.toggle("npcBubble", true);
@@ -141,17 +117,14 @@ function set_direction(bubbleElement, isByPlayer) {
     }
 }
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 function check_progression(progressionInt) {
     if (progressionInt == 2)
-        check_branchingSession(progressionInt, "caras gourmet")
+        check_branchingSession(progressionInt, "caras gourmet");
     else
         load_txtChatSession(progressionInt);
 }
 
+//check message for an alternative keyword, if not found in previous .txt-file
 function check_branchingSession(startSessionInt, primaryKeyword) {
     var tempProgressionInt = startSessionInt;
     if (tempInput.toLowerCase().includes(primaryKeyword))
@@ -160,16 +133,18 @@ function check_branchingSession(startSessionInt, primaryKeyword) {
         load_txtChatSession(tempProgressionInt);
 }
 
+//loading npc's message collection, after the player wrote a message
 function load_txtChatSession(sessionInt) {
     var txtFile = new XMLHttpRequest();
     txtFile.open("GET", "assets/txt/session_" + String(sessionInt) + ".txt", true);
     txtFile.onreadystatechange = function () {
         if (txtFile.readyState === 4 && txtFile.status == 200)
             parse_chatSession(txtFile.responseText, sessionInt);
-    }
+    };
     txtFile.send(null);
 }
 
+//checking, whether the player's last message contains a keyword or not
 function parse_chatSession(txtContent, sessionInt) {
     var keywords = txtContent.split("KEYWORDS: [[")[1].split("]]")[0].split(", ");
     var npcMessages = txtContent.split("KEYWORDS: [[")[1].split("]]")[1].split(">> ");
@@ -179,15 +154,6 @@ function parse_chatSession(txtContent, sessionInt) {
         delete_playerMessage(latestPlayerMessage);
 }
 
-function npcReaction(npcMessages, sessionInt) {
-    sleep(messageTime * 1000).then(() => {
-        isWriting = true;
-        statusLine.innerHTML = "Online";
-        send_npcMessages(npcMessages, 1);
-        progressionInt = sessionInt + 1;
-    })
-}
-
 function check_keyword(message, keywords) {
     for (var element = 0; element < keywords.length; element++)
         if (message.toLowerCase().includes(keywords[element]))
@@ -195,13 +161,37 @@ function check_keyword(message, keywords) {
     return false;
 }
 
-function check_spacesOnly(message) {
-    for (var element = 0; element < message.length(); element++)
-        if (message[element] != " ")
-            return false;
-    return true;
+//if keyword is contained: print npc's messages
+function npcReaction(npcMessages, sessionInt) {
+    isWriting = true;
+    sleep(messageTime * 1000).then(() => {
+        statusLine.innerHTML = "Online";
+        send_npcMessages(npcMessages, 1);
+        progressionInt = sessionInt + 1;
+    });
 }
 
+//recursive function: printing the npc's messages one after one
+function send_npcMessages(messageArray, messageInt) {
+    sleep(((messageTime - Math.random()) + (messageTime + Math.random())) * 500).then(() => {
+        statusLine.innerHTML = "<i>Writing...</i>";
+        if (messageInt < messageArray.length) {
+            sleep((messageArray[messageInt].length) * 1).then(() => {
+                create_bubble(messageArray[messageInt], false);
+                send_npcMessages(messageArray, ++messageInt);
+            });
+        }
+        else {
+            isWriting = false;
+            statusLine.innerHTML = "Online";
+            sleep(onlineAfter * 1000).then(() => {
+                statusLine.innerHTML = "";
+            });
+        }
+    });
+}
+
+//if no keyword is contained: erase player's last message
 function delete_playerMessage(elementBubble) {
     elementBubble.style.transitionDuration = String(deleteTransitionTime) + "s";
     elementBubble.style.transitionDelay = "1s";
@@ -210,20 +200,22 @@ function delete_playerMessage(elementBubble) {
         elementBubble.style.opacity = 0;
         sleep(deleteTransitionTime * 1500).then(() => {
             elementBubble.remove();
-        })
-    })
+        });
+    });
 }
 
+//loading chatHistory.txt, which is the dialogue of the past days found in the chat above
 function load_txtHistory() {
     var txtFile = new XMLHttpRequest();
     txtFile.open("GET", "assets/txt/history.txt", true);
     txtFile.onreadystatechange = function () {
         if (txtFile.readyState === 4 && txtFile.status == 200)
             parse_chatHistory(txtFile.responseText);
-    }
+    };
     txtFile.send(null);
 }
 
+//determining, whether the chatHistory's messages belong to npc or player character
 function parse_chatHistory(txtContent) {
     var allMessages = txtContent.split(">>");
     for (var message = 1; message <= allMessages.length - 1; message++) {
@@ -236,8 +228,9 @@ function parse_chatHistory(txtContent) {
     }
 }
 
+//determining chatHistory's dates for the timeBubbles
 function get_passedDate(passedDaysInt) {
     var date = new Date();
-    date.setDate(date.getDate() - passedDaysInt)
+    date.setDate(date.getDate() - passedDaysInt);
     return String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear();
 }
